bee21a4af14e1c11a246b99d98e67cd692f5408a
lab_vjezbe <lab@oss>
2020-03-10 18:14:09 +0100

    vjezba2

---
+++ vjezba2/zadatak3/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int& funk(int* niz, int i)
+{
+    return niz[i];
+}
+int main()
+{
+    int n=4;
+    int niz[]={1,2,3,4};
+    int i=2;
+    funk(niz,i)+=1;
+    for(int i=0;i<n;i++)
+    {
+       cout<<niz[i]<<",";
+    }
+}
